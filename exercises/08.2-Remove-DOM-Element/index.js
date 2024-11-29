60b7f931c75ca3389fedc5c66d1da042fc83edfe
Alejandro <alejandronicolasdelalamo@gmail.com>
2024-11-29 09:15:42 +0000

    First commit

--- exercises/08.2-Remove-DOM-Element/index.js
+++ exercises/08.2-Remove-DOM-Element/index.js
@@ -1,1 +1,3 @@
 // Your code here
+let li = document.querySelector("#secondElement")
+li.parentNode.removeChild(li)
--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,3 +1,29 @@
-let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
+const inputField = document.getElementById("addToDo");
+const todoList = document.querySelector("ul");
 
-// Your code here
+function addTask() {
+  const taskText = inputField.value.trim();
+
+  if (taskText !== "") {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `<span><i class="fa fa-trash"></i></span> ${taskText}`;
+    todoList.appendChild(listItem);
+    inputField.value = "";
+  }
+}
+
+function deleteTask(event) {
+  const clickedElement = event.target;
+  if (clickedElement.classList.contains("fa-trash")) {
+    const listItem = clickedElement.parentElement.parentElement;
+    todoList.removeChild(listItem);
+  }
+}
+
+inputField.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+todoList.addEventListener("click", deleteTask);
